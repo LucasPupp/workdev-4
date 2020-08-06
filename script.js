b49b7bfd6e0c9fe12cd04212cf7ab1c6c61b97de
Bruno Marconcini PUpp <brunopupp.bp@gmail.com>
2020-08-06 09:53:53 -0300

    script adicionado

---
+++ script.js
@@ -0,0 +1,18 @@
+function onOFF(){
+  document
+    .querySelector("#modal")
+    .classList
+    .toggle("hide")
+
+  
+  document
+      .querySelector("body")
+      .classList
+      .toggle("hideScroll")
+
+  
+   document
+      .querySelector("#modal")
+      .classList
+      .toggle("addScroll")
+}
